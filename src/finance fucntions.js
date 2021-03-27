0f576f122a4bf1a55c98e8418b0fc0f8f3125586
robertb945 <63433811+robertb945@users.noreply.github.com>
2021-03-27 14:16:29 -0500

    Update finance fucntions.js

---
+++ src/finance fucntions.js
@@ -0,0 +1,44 @@
+/*
+Functions used to calculate the financial components ofthe data set
+*/
+
+//Growth of the investment principal 
+export function CompoundInterest(principal , rate, periods, years) {
+
+	return principal *  ((1 + (rate/periods))**(periods * years));
+}
+
+//Growth of contributions to an investment
+export function FutureValueSeries(annual_contribution, rate, periods, years){
+
+   return annual_contribution * ((((1 + (rate/periods))**(periods * years)) -1) / (rate/periods))
+}
+
+
+//Growth of a retirement account with age based contribution restrictions (410k, Roth 401k, IRA, Roth IRA)
+export function retirementAccountGrowth(person_age, person_retirement_age, principal, annual_contribution, catchup_contribution, rate, periods, years, withdraw_rate){
+
+    if (person_age < 50) {
+        //Contributions without catchup contributions
+        //No withdraws
+
+        contributions = annual_contribution
+        return (CompoundInterest(principal , rate, periods, years) + FutureValueSeries(contributions, rate, periods, years))
+
+    } else if (person_age >= 50 && person_age < person_retirement_age) {
+        //Contributions with catchup contributions
+        //No withdraws
+
+        contributions = annual_contribution + catchup_contribution
+        return (CompoundInterest(principal , rate, periods, years) + FutureValueSeries(contributions, rate, periods, years))
+
+    } else if (person_age >= person_retirement_age){
+        //No further contributions
+        //withdraws reduce the amount
+
+        return (CompoundInterest(principal , rate, periods, years) - (principal * withdraw_rate))
+
+    } else {
+        //error case
+    }
+}
