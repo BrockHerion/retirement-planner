78b0fdeeea4619b64d9a568ef4ce611abc1ec0cb
Carson Christ <carsonjesus@gmail.com>
2021-04-21 14:08:04 -0500

    Added Data Manager

--- src/config/addAccount.js
+++
@@ -1,7 +0,0 @@
-const Account = require("../models/Account.js")
-
-function addAccount(guipersonIDd, accountName, type, balance, annual_contribution, catchup_contribution) {
-
-    let newAccount = new Account(guipersonIDd, accountName, type, balance, annual_contribution, catchup_contribution)
-
-}
