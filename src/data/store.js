0ab2fac3cde7e9cb2a7eed2a7f444710ea04386f
Brock Herion <brockheriondev@gmail.com>
2021-04-22 17:48:22 -0500

    Added record reducer

--- src/data/store.js
+++ src/data/store.js
@@ -1,10 +1,12 @@
 const { configureStore } = require('@reduxjs/toolkit')
 
 const person = require('./slices/person')
+const record = require('./slices/record')
 
 const store = configureStore({
   reducer: {
     persons: person.reducer,
+    records: record.reducer
   }
 })
 
